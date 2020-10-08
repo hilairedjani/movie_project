0ddd4ea0661525a889cc227239c75415e9732b7a
hilairedjani <djanilewis1@gmail.com>
2020-10-07 21:10:21 -0400

    - Fetched movies from db

---
+++ client/assets/javascript/index.js
@@ -0,0 +1,42 @@
+function init() {
+  fetchTopMovies();
+}
+
+function fetchTopMovies() {
+  let topMovies;
+  let request = new XMLHttpRequest();
+
+  request.onreadystatechange = function () {
+    if (this.readyState == 4 && this.status == 200) {
+      topMovies = JSON.parse(this.responseText);
+      displayTopMovies(topMovies);
+    }
+  };
+
+  request.open("GET", "/top-movies");
+  request.send();
+}
+
+function displayTopMovies(movies) {
+  const moviesContainer = $("#top-movies");
+  const moviesIndicators = $("#top-movies-carousel-indicators");
+  const moviesCarouselInner = $("#top-movies-carousel-inner");
+
+  for (let i = 0; i < movies.length; i++) {
+    moviesIndicators.append(
+      `<li data-target="#top-movies" data-slide-to="${i}"></li>`
+    );
+
+    moviesCarouselInner.append(
+      `<div class="carousel-item ${i == 0 ? "active" : ""}">
+        <img src="${
+          movies[i].Poster
+        }" class="d-block w-100" height="750 alt="...">
+        <div class="carousel-caption d-none d-md-block bg-dark">
+            <h5>${movies[i].Title}</h5>
+            <p>${movies[i].Plot}</p>
+        </div>
+    </div>`
+    );
+  }
+}
--- server.js
+++ server.js
@@ -3,6 +3,8 @@ const fs = require("fs");
 
 const PORT = 3000;
 
+const topMovies = require("./db/movie-data-short.json");
+
 function executeGet({
   fileName,
   fileType = "text/html",
@@ -27,15 +29,53 @@ function executeGet({
 const server = http.createServer((request, response) => {
   const method = request.method;
   const url = request.url.slice(1, request.url.length);
-
+  console.log(url);
   switch (method) {
     case "GET":
       switch (url) {
         case "movies":
           break;
 
+        case "top-movies":
+          response.statusCode = 200;
+          response.setHeader("Content-Type", "text/html");
+          response.write(JSON.stringify(topMovies));
+          response.end();
+          break;
+
+        case "assets/javascript/index.js":
+          fs.readFile("./client/assets/javascript/index.js", function (
+            err,
+            data
+          ) {
+            if (err) {
+              console.log("HTML Route Error");
+              response.statusCode = 500;
+              response.write("Server Error");
+              response.end();
+            }
+
+            response.statusCode = 200;
+            response.setHeader("Content-Type", "application/javascript");
+            response.write(data);
+            response.end();
+          });
+          break;
+
         default:
-          executeGet({ fileName: "client/index.html", response: response });
+          fs.readFile("./client/src/index.html", function (err, data) {
+            if (err) {
+              console.log("HTML Route Error");
+              response.statusCode = 500;
+              response.write("Server Error");
+              response.end();
+            }
+
+            response.statusCode = 200;
+            response.setHeader("Content-Type", "text/html");
+            response.write(data);
+            response.end();
+          });
           break;
       }
       break;
