72d8a95969f600406c5dc2660459da56d2d539c6
Evo55 <ebubeokelekwe@gmail>
2020-12-03 19:15:11 -0500

    scraperr

--- Helpers/index.js
+++ Helpers/index.js
@@ -1,8 +1,8 @@
 const fetch = require('node-fetch');
-const Omdb_Url = `http://www.omdbapi.com/?i=tt3896198&apikey=36533b13`;
+const Omdb_Url = `http://www.omdbapi.com/?apikey=36533b13`;
 exports.fetchmoviefromapi = async (movie) => {
     try {
-        const data = await fetch(`${Omdb_Url}&${movie}`)
+        const data = await fetch(`${Omdb_Url}&t=${movie}`)
         const response = await data.text()
         console.log(response)
         return response
